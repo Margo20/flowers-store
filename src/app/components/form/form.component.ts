46f71a8b5bde6bde8ecfcc06eaddd1db1002330e
Margarita Aniskovets <rita.aniskovets@gmail.com>
2020-07-30 11:14:30 +0300

    jhfjfjhgfjg

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-form',
@@ -10,6 +11,7 @@ export class FormComponent implements OnInit {
   @Input()
   id: string;
   signInForm: FormGroup;
+  // allEvents = allEvents;
 
   constructor(
     private formBuilder: FormBuilder
@@ -17,13 +19,20 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
     this.signInForm = this.formBuilder.group({
-      login: [null],
-      pass: [null]
+      // login: [null, [Validators.required()]],
+      // pass: [null,[Validators.minLength(5)],[Validators.maxLength(15)]],
+      // email: [null,[Validators.email()]],
+
     });
   }
 
   onSignInSubmit(event: Event) {
     event.preventDefault();
+
+    if(!this.signInForm.valid) {
+      return;
+    }
+    // this.userService.user.next({login: this.signInForm.value.login})
 
     console.log(this.signInForm.value);
   }
